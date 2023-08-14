152de5eb90aabad593a6fe329caf518ce559b60e
Chetan kumar <chetankumar71297@gmail.com>
2023-08-14 22:01:41 +0530

    Create home page with carousel
    Used tailwind css component and react alice carousel.

---
+++ src/customer/components/HomeCarousel/MainCarousel.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import { mainCarouselData } from "./MainCarouselData";
+
+const items = mainCarouselData.map((item) => (
+  <img src={item.image} className="cursor-pointer" role="presentation" alt="" />
+));
+
+const MainCarousel = () => (
+  <AliceCarousel
+    items={items}
+    disableButtonsControls
+    autoPlay
+    autoPlayInterval={1000}
+    infinite
+  />
+);
+export default MainCarousel;
---
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import MainCarousel from "../../components/HomeCarousel/MainCarousel";
+
+export default function HomePage() {
+  return (
+    <div>
+      <MainCarousel />
+      <div>other section</div>
+    </div>
+  );
+}
